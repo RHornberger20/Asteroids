fcc558843c5d821e4bf3446f915debb01255d9de
RHornberger20 <ryan.hornberger@gmail.com>
2025-11-01 12:42:16 -0500

    Adding FPS so game runs smoothly

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from constants import *
 def main():
 
 	pygame.init()
+
+	clock = pygame.time.Clock()
+	dt = 0
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 
@@ -13,6 +16,8 @@ def main():
 				return
 		screen.fill("black")
 		pygame.display.flip()
+		dt = clock.tick(60)/1000
+
 	print("Starting Asteroids!")
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
