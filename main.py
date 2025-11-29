923d341f0e231525089a7ac4a2fb09d5750c148b
RHornberger20 <ryan.hornberger@gmail.com>
2025-11-29 09:47:53 -0600

    Added updatable and drawable groups to main

--- main.py
+++ main.py
@@ -9,6 +9,12 @@ def main():
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
 
+
+	updatable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
+
+	Player.containers = (updatable, drawable)
+
 	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 	pygame.init()
 
@@ -16,15 +22,26 @@ def main():
 	dt = 0
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-	while 1:
+
+
+	while True:
 		log_state()
+
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
+
+		updatable.update(dt)
+
 		screen.fill("black")
-		player.draw(screen)
+
+		for obj in drawable:
+			obj.draw(screen)
+
 		pygame.display.flip()
+
 		dt = clock.tick(60)/1000
+
 
 	print("Starting Asteroids!")
 	print(f"Screen width: {SCREEN_WIDTH}")
